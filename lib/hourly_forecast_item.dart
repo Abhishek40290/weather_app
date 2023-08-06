209910b5dc35355081c465e38e2eab99ac008a3c
Abhishek Kumar <abhishek40290@gmail.com>
2023-08-06 22:45:20 +0530

    initial commit

---
+++ lib/hourly_forecast_item.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class HourlyForecastItem extends StatelessWidget {
+  final String time;
+  final String temperature;
+  final IconData icon;
+  const HourlyForecastItem({
+    super.key,
+    required this.time,
+    required this.temperature,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      child: Container(
+        width: 100,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text(
+                time,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              const SizedBox(height: 8),
+              Icon(
+                icon,
+                size: 32,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                temperature,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/secrets.dart
@@ -0,0 +1,1 @@
+const openWeatherApiKey = '602fbfd19f2d2a900868e9a5893b436d';
